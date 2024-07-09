e5db2b6ddeea54e8d667cee42332caafd3b04967
kishan kathare <kishankathare45@gmail.com>
2024-07-10 03:31:03 +0530

    basic middleware added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,29 @@
 import "dotenv/config"
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
+import morgan from "morgan";
+import { isHttpError } from 'http-errors';
 
 import UserModel from "./models/user";
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
+
 app.get("/", async (req, res) => {
     const users = await UserModel.find().exec();
     res.status(200).json(users);
+})
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An Unknown Error Occured";
+    let errorStatus = 500;
+    if (isHttpError(error)) {
+        errorMessage = error.message;
+        errorStatus = error.status;
+    }
+    res.status(errorStatus).json({error: errorMessage});
 })
 
 export default app;
