af21a4de2911ef93cd379be526dbf10ba54c9cf7
kishan kathare <kishankathare45@gmail.com>
2024-07-16 00:19:42 +0530

    Frontend: Empty Dashboard Component Added

---
+++ frontend/src/components/Dashboard/EmptyDashboard.tsx
@@ -0,0 +1,9 @@
+const EmptyDashboard = () => {
+  return (
+    <h1 className="text-center text-7xl text-[#EAEAEA]/30 font-montserrat font-extrabold">
+      Empty Workspace
+    </h1>
+  )
+}
+
+export default EmptyDashboard
--- frontend/src/components/Dashboard/UserDashboard.tsx
+++ frontend/src/components/Dashboard/UserDashboard.tsx
@@ -1,10 +1,14 @@
 import NeonButton from "../Buttons/NeonButton"
+import EmptyDashboard from "./EmptyDashboard"
 
 const UserDashboard = () => {
   return (
     <div className="background px-36 py-24">
       <div className="w-full flex justify-end">
         <NeonButton text="Create NoteBook" />
+      </div>
+      <div className="w-full h-full grid place-items-center">
+        <EmptyDashboard />
       </div>
     </div>
   )
